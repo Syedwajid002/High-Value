18df347d6f0146668c7556ee2e215851666e87b2
Wajid002 <syedwajid002@gmail.com>
2025-06-22 22:25:42 +0530

    location change

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -58,7 +58,7 @@ const Footer: React.FC = () => {
                 <MapPin size={20} className="text-primary-400 shrink-0 mt-1" />
                 <div>
                   <p>Multiple Locations Across the globe</p>
-                  <span>Warangal</span>
+                  <span>Gopalpur Hanamkonda District</span>
                 </div>
               </div>
               <div className="flex items-center gap-3">
